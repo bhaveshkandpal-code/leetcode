56331f1d8c638d36e9a76682b36bb6bc1dfeeacc
bhavesh <bhaveshkandpal88@gmail.com>
2026-07-07 22:15:03 +0530

    LC-concatenate-non-zero-digits-and-multiply-by-sum-i [Runtime: N/A; Memory: N/A]

--- math/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ math/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    long long concatAndMultiply(int n) {
-        vector<int> digits;
+    long long sumAndMultiply(int n) {
+        long long concat = 0;
+        long long sum = 0;
 
-        while (n > 0) {
-            if (n % 10 != 0)
-                digits.push_back(n % 10);
-            n /= 10;
-        }
+        string s = to_string(n);
 
-        reverse(digits.begin(), digits.end());
-
-        long long concat = 0, sum = 0;
-        for (int d : digits) {
-            concat = concat * 10 + d;
-            sum += d;
+        for (char c : s) {
+            if (c != '0') {
+                int d = c - '0';
+                concat = concat * 10 + d;
+                sum += d;
+            }
         }
 
         return concat * sum;
