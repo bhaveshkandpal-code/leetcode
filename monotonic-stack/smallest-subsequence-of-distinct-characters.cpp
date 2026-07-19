49d4557aa92fb38794df957cfc2adb772382db9c
bhavesh <bhaveshkandpal88@gmail.com>
2026-07-19 23:12:13 +0530

    LC-smallest-subsequence-of-distinct-characters [Runtime: N/A; Memory: N/A]

---
+++ monotonic-stack/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> last(26), vis(26, 0);
+        
+        for (int i = 0; i < s.size(); i++)
+            last[s[i] - 'a'] = i;
+        
+        string st;
+        
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            
+            if (vis[c - 'a'])
+                continue;
+            
+            while (!st.empty() && st.back() > c && last[st.back() - 'a'] > i) {
+                vis[st.back() - 'a'] = 0;
+                st.pop_back();
+            }
+            
+            st.push_back(c);
+            vis[c - 'a'] = 1;
+        }
+        
+        return st;
+    }
+};
