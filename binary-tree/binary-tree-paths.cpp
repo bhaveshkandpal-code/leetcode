afbb114faaa1736508677e95dce3437b5920bc52
bhavesh <bhaveshkandpal88@gmail.com>
2026-07-19 23:17:25 +0530

    LC-binary-tree-paths [Runtime: N/A; Memory: N/A]

--- binary-tree/binary-tree-paths.cpp
+++ binary-tree/binary-tree-paths.cpp
@@ -1,24 +1,24 @@
-vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-    vector<vector<int>> res;
-    int i = 0, n = intervals.size();
+class Solution {
+public:
+    vector<string> ans;
 
-    while (i < n && intervals[i][1] < newInterval[0]) {
-        res.push_back(intervals[i]);
-        i++;
+    void dfs(TreeNode* root, string path) {
+        if (!root) return;
+
+        if (!path.empty()) path += "->";
+        path += to_string(root->val);
+
+        if (!root->left && !root->right) {
+            ans.push_back(path);
+            return;
+        }
+
+        dfs(root->left, path);
+        dfs(root->right, path);
     }
 
-    while (i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = min(newInterval[0], intervals[i][0]);
-        newInterval[1] = max(newInterval[1], intervals[i][1]);
-        i++;
+    vector<string> binaryTreePaths(TreeNode* root) {
+        dfs(root, "");
+        return ans;
     }
-
-    res.push_back(newInterval);
-
-    while (i < n) {
-        res.push_back(intervals[i]);
-        i++;
-    }
-
-    return res;
-}
+};
