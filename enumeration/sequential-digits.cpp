9a98a8b1789fb327f56afcf865368bfa3f567352
bhavesh <bhaveshkandpal88@gmail.com>
2026-07-26 00:09:33 +0530

    LC-sequential-digits [Runtime: N/A; Memory: N/A]

---
+++ enumeration/sequential-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string s = "123456789";
+        vector<int> ans;
+
+        int l1 = to_string(low).size();
+        int l2 = to_string(high).size();
+
+        for (int len = l1; len <= l2; len++) {
+            for (int i = 0; i + len <= 9; i++) {
+                int num = stoi(s.substr(i, len));
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
