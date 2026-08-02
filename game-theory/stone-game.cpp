0eade09c7c690b44aa490ee18d86a07bac484125
bhavesh <bhaveshkandpal88@gmail.com>
2026-08-02 22:49:53 +0530

    LC-stone-game [Runtime: N/A; Memory: N/A]

---
+++ game-theory/stone-game.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+     return true ;   // its optimaization problem , so both will make move to gain more numebr , so whoever will make first move that will be the winner   
+    }
+};
