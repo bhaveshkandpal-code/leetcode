74ed8cf759e3cc1d4fb5d6135e4d5335d023929b
bhavesh <bhaveshkandpal88@gmail.com>
2026-08-04 23:50:00 +0530

    LC-find-missing-elements [Runtime: N/A; Memory: N/A]

---
+++ array/find-missing-elements.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int findMissingElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int expected = n * (n + 1) / 2;
+        int actual = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            actual += nums[i];
+        }
+
+        return expected - actual;
+    }
+};
