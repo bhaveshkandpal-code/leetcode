c76c8525cadd3ceaca69eb822ba03d7fc37cdf8b
bhavesh <bhaveshkandpal88@gmail.com>
2026-08-18 22:34:12 +0530

    LC-find-the-largest-almost-missing-integer [Runtime: N/A; Memory: N/A]

--- hash-table/find-the-largest-almost-missing-integer.cpp
+++ hash-table/find-the-largest-almost-missing-integer.cpp
@@ -2,27 +2,40 @@ class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> count(51, 0);
-
-        for (int x : nums)
+        unordered_map<int, int> count;
+        for (int x : nums) {
             count[x]++;
+        }
 
+        // Case 1: Subarray size is 1
         if (k == 1) {
             int ans = -1;
-            for (int x : nums) {
-                if (count[x] == 1)
-                    ans = max(ans, x);
+            for (auto& [val, freq] : count) {
+                if (freq == 1) {
+                    ans = max(ans, val);
+                }
             }
             return ans;
         }
 
+        // Case 2: Subarray size equals array length
+        if (k == n) {
+            int ans = -1;
+            for (int x : nums) {
+                ans = max(ans, x);
+            }
+            return ans;
+        }
+
+        // Case 3: 1 < k < n
+        // Only nums[0] or nums[n-1] can potentially appear in exactly 1 subarray of length k.
         int ans = -1;
-
-        if (count[nums[0]] == 1)
+        if (count[nums[0]] == 1) {
             ans = max(ans, nums[0]);
-
-        if (count[nums[n - 1]] == 1)
+        }
+        if (count[nums[n - 1]] == 1) {
             ans = max(ans, nums[n - 1]);
+        }
 
         return ans;
     }
