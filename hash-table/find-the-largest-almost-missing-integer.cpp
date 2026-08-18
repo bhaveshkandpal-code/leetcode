e984e442c5906bf8992cbe06c3a5c17bb0c76856
bhavesh <bhaveshkandpal88@gmail.com>
2026-08-18 22:34:37 +0530

    LC-find-the-largest-almost-missing-integer [Runtime: N/A; Memory: N/A]

--- hash-table/find-the-largest-almost-missing-integer.cpp
+++ hash-table/find-the-largest-almost-missing-integer.cpp
@@ -7,7 +7,6 @@ public:
             count[x]++;
         }
 
-        // Case 1: Subarray size is 1
         if (k == 1) {
             int ans = -1;
             for (auto& [val, freq] : count) {
@@ -18,7 +17,6 @@ public:
             return ans;
         }
 
-        // Case 2: Subarray size equals array length
         if (k == n) {
             int ans = -1;
             for (int x : nums) {
@@ -27,8 +25,6 @@ public:
             return ans;
         }
 
-        // Case 3: 1 < k < n
-        // Only nums[0] or nums[n-1] can potentially appear in exactly 1 subarray of length k.
         int ans = -1;
         if (count[nums[0]] == 1) {
             ans = max(ans, nums[0]);
