c1e2b1effa77316ad6311b3c1554c74332cc4592
bhavesh <bhaveshkandpal88@gmail.com>
2026-09-01 22:21:06 +0530

    LC-minimum-moves-to-clean-the-classroom [Runtime: 1100 ms; Memory: 390.5 MB]

---
+++ bit-manipulation/minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,100 @@
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        int m = classroom.size();
+        int n = classroom[0].size();
+
+        vector<vector<int>> id(m, vector<int>(n, -1));
+
+        int sr = 0, sc = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (classroom[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                }
+                else if (classroom[i][j] == 'L') {
+                    id[i][j] = cnt++;
+                }
+            }
+        }
+
+        if (cnt == 0) return 0;
+
+        int fullMask = (1 << cnt) - 1;
+
+        // r, c, remaining energy, collected litter mask
+        queue<tuple<int, int, int, int>> q;
+
+        q.push({sr, sc, energy, 0});
+
+        vector<vector<vector<vector<bool>>>> vis(
+            m,
+            vector<vector<vector<bool>>>(
+                n,
+                vector<vector<bool>>(
+                    energy + 1,
+                    vector<bool>(1 << cnt, false)
+                )
+            )
+        );
+
+        vis[sr][sc][energy][0] = true;
+
+        int dist = 0;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+
+            int sz = q.size();
+
+            while (sz--) {
+                auto [r, c, e, mask] = q.front();
+                q.pop();
+
+                if (mask == fullMask)
+                    return dist;
+
+                if (e == 0)
+                    continue;
+
+                for (int k = 0; k < 4; k++) {
+
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+
+                    if (nr < 0 || nr >= m ||
+                        nc < 0 || nc >= n)
+                        continue;
+
+                    if (classroom[nr][nc] == 'X')
+                        continue;
+
+                    int ne = e - 1;
+                    int nmask = mask;
+
+                    // Reset energy
+                    if (classroom[nr][nc] == 'R')
+                        ne = energy;
+
+                    // Collect litter
+                    if (classroom[nr][nc] == 'L')
+                        nmask |= (1 << id[nr][nc]);
+
+                    if (!vis[nr][nc][ne][nmask]) {
+                        vis[nr][nc][ne][nmask] = true;
+                        q.push({nr, nc, ne, nmask});
+                    }
+                }
+            }
+
+            dist++;
+        }
+
+        return -1;
+    }
+};
