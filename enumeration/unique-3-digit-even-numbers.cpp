ef3c9f7d45916f53d2fdc03d34fdbf7a9252c5b1
bhavesh <bhaveshkandpal88@gmail.com>
2026-09-11 23:43:38 +0530

    LC-unique-3-digit-even-numbers [Runtime: N/A; Memory: N/A]

---
+++ enumeration/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        unordered_set<int> s;
+        int n = digits.size();
+
+        for (int i = 0; i < n; i++) {
+            if (digits[i] % 2) continue;
+
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                for (int k = 0; k < n; k++) {
+                    if (k == i || k == j || digits[k] == 0) continue;
+
+                    s.insert(digits[k] * 100 + digits[j] * 10 + digits[i]);
+                }
+            }
+        }
+
+        return s.size();
+    }
+};
