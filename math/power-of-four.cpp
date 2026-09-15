349ae49a18526a46b2d6c713e0e99f5683f62d0e
bhavesh <bhaveshkandpal88@gmail.com>
2026-09-15 23:09:00 +0530

    LC-power-of-four [Runtime: N/A; Memory: N/A]

--- math/power-of-four.cpp
+++ math/power-of-four.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        
-      
+
         if (n <= 0)
             return false;
 
-        
-        if ((n - 1) % 3 == 0)
-            return true;
+        if ((n & (n - 1)) != 0)
+            return false;
 
-        return false;
+        if ((n - 1) % 3 != 0)
+            return false;
+
+        return true;
     }
 };
