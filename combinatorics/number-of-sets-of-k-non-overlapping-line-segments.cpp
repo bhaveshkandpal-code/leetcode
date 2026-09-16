4afcf8a89d9ec87736c83567f4ca8f6f73de2d46
bhavesh <bhaveshkandpal88@gmail.com>
2026-09-16 22:37:28 +0530

    LC-number-of-sets-of-k-non-overlapping-line-segments [Runtime: N/A; Memory: N/A]

--- combinatorics/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ combinatorics/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,22 +1,25 @@
-class Solution {
-public:
-    int numberOfSets(int n, int k) {
-        const int MOD = 1e9 + 7;
+class Solution:
+    def numberOfSets(self, n: int, k: int) -> int:
+        MOD = 10**9 + 7
 
-        vector<vector<long long>> dp(n, vector<long long>(k + 1, 0));
+        # dp[j] = ways with j completed segments
+        # prefix[j] = accumulated ways that can be extended
+        dp = [0] * (k + 1)
+        prefix = [0] * (k + 1)
 
-        for (int i = 0; i < n; i++)
-            dp[i][0] = 1;
+        dp[0] = 1
+        prefix[0] = 1
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= k; j++) {
-                dp[i][j] = dp[i - 1][j];
+        for _ in range(1, n):
+            new_dp = dp[:]
 
-                if (i >= 1)
-                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
-            }
-        }
+            for j in range(1, k + 1):
+                # End a new segment at the current point.
+                new_dp[j] = (dp[j] + prefix[j - 1]) % MOD
 
-        return dp[n - 1][k];
-    }
-};
+            dp = new_dp
+
+            for j in range(k + 1):
+                prefix[j] = (prefix[j] + dp[j]) % MOD
+
+        return dp[k]
